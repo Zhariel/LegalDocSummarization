b5791acfe243cb2f16588e150480bfbba13c6b89
Romain Blazevic <romain.blazevic@gmail.com>
2022-12-31 15:20:12 +0100

    finished appli

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request
-from nlp.train import train_model
+from nlp.train import train_model, class_names_long
 from nlp.summarize import summarize_contract
 import os
 
-naive_bayes = train_model(sentences_path=os.path.join('nlp', 'sentences'), tags_path=os.path.join('nlp', 'tags'))
-print(type(naive_bayes))
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    if 'text' in request.args:
-        print(request.args['text'])
-        summary = summarize_contract(model=naive_bayes, document=request.args['text'])
-        return render_template('summary.html', summary=summary)
-
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+# naive_bayes = train_model(sentences_path=os.path.join('nlp', 'sentences'), tags_path=os.path.join('nlp', 'tags'))
+# print(type(naive_bayes))
+#
+# app = Flask(__name__)
+#
+# @app.route('/', methods=['GET'])
+# def index():
+#     if 'text' in request.args:
+#         print(request.args['text'])
+#         summary = summarize_contract(model=naive_bayes, document=request.args['text'])
+#         return render_template('summary.html', summary=summary)
+#
+#     return render_template('index.html')
+#
+#
+# if __name__ == '__main__':
+#     app.run()
--- nlp/summarize.py
+++ nlp/summarize.py
@@ -1,20 +1,7 @@
 from nltk.tokenize import TreebankWordTokenizer
-from nltk.corpus import movie_reviews
-import nltk
-import random
-from os import listdir
-from os.path import isfile, join
-from nltk.corpus import stopwords
-import string
-from sklearn.tree import DecisionTreeClassifier
-from nltk.tokenize import word_tokenize
-from random import shuffle
-from nltk import classify
 from nltk import NaiveBayesClassifier
-from nltk.corpus import wordnet
-import os
 import re
-from nlp.train import bag_of_words, class_names
+from nlp.train import bag_of_words, class_names, class_names_long
 
 def summarize_contract(model: NaiveBayesClassifier, document: str):
     letters_sub = r'[^a-zA-Z.]'
@@ -24,6 +11,7 @@ def summarize_contract(model: NaiveBayesClassifier, document: str):
     text = [t for t in text if len(t) > 2]
     tokenizer = TreebankWordTokenizer()
     classes = class_names()
+    classes_long = class_names_long()
 
     kept_sentences = []
 
@@ -32,6 +20,7 @@ def summarize_contract(model: NaiveBayesClassifier, document: str):
         bag = bag_of_words(processed_sentence)
         predicted_class = model.classify(bag)
         if predicted_class in classes:
-            kept_sentences.append((predicted_class, sentence))
+            kept_sentences.append((classes_long[predicted_class], sentence))
 
     return kept_sentences
+
